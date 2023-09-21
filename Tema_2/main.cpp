069e0b42b20bb518e279a7c2f81772274dfcbb33
ProReaps <angeldejesussandovalbrito@gmail.com>
2023-09-21 14:07:27 -0700

    Tema_2 con el código base (de la clase)

--- Tema_2/main.cpp
+++ Tema_2/main.cpp
@@ -1,68 +1,59 @@
+#include <Box2D/Box2D.h>
+#include <cmath>
 #include <iostream>
-#include <Box2d/Box2d.h>
-//Descripcion:
-//Tiro parabólico
 
-int main(){
-    //Mundo
-    //Creando el vector de gravedad y el mundo
-    //En este caso la gravedad es -24.79f porque está basado en las condiciones de jupiter
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+int main() {
+    // Define the gravity vector
     b2Vec2 gravity(0.0f, -9.81f);
+
+    // Construct a world object, which will hold and simulate the rigid bodies
     b2World world(gravity);
 
-
-    //Piso (no cambié nada del código de la clase en la sección de piso)
-    //Características del cuerpo
-    b2BodyDef groundBodyDef;
-    groundBodyDef.position.Set(0.0f, -10.0f);
-
-    //Creación del cuerpo
-    b2Body* groundBody = world.CreateBody(&groundBodyDef);
-
-    //Crear la forma del cuerpo
-    b2PolygonShape groundBox;
-    groundBox.SetAsBox(50,1);
-
-    //Se crea el fixture
-    groundBody->CreateFixture(&groundBox, 0.0f);
-
-
-    //Cuerpo dinamico (caja)
-    //Características de la caja
+    // Create a dynamic body for the bullet
     b2BodyDef bodyDef;
     bodyDef.type = b2_dynamicBody;
-    bodyDef.position.Set(0.0f,15.0f);   //cambia la altura a la dada para el ejercicio
-
-    //Creacion del cuerpo
+    bodyDef.position.Set(0.0f, 0.0f);
     b2Body* body = world.CreateBody(&bodyDef);
 
-    //Crear la forma del cuerpo
+    // Define another box shape for the bullet
     b2PolygonShape dynamicBox;
-    dynamicBox.SetAsBox(1.0f,1.0f);
+    dynamicBox.SetAsBox(0.2f, 0.05f);
 
-    //Hacemos una fixture def aparte para cambiar cada característica del cuerpo
+    // Define the dynamic body fixture
     b2FixtureDef fixtureDef;
     fixtureDef.shape = &dynamicBox;
-    fixtureDef.density = 15600.0f;  //densidad cambiada a la del carburo de tungsteno
-    //fricción del aire (inventada pero mayor)
-    fixtureDef.friction = 0.9f;
+    fixtureDef.density = 0.01f;
+    fixtureDef.friction = 0.3f;
 
-    //Cargamos el fixtureDef que creamos
+    // Add the shape to the body
     body->CreateFixture(&fixtureDef);
 
+    // Set the initial velocity of the bullet
+    float angle = 45.0f; // Angle in degrees
+    float speed = 400.0f; // Speed in m/s
+    float vx = speed * std::cos(angle * M_PI / 180.0f);
+    float vy = speed * std::sin(angle * M_PI / 180.0f);
 
-    //Caida
-    float timeStep = 1.0f/60;
+    body->SetLinearVelocity(b2Vec2(vx, vy));
 
+    // Simulate the world
+    float timeStep = 1.0f / 60.0f;
     int32 velocityIterations = 6;
-    int32 positionIteration = 2;
+    int32 positionIterations = 2;
 
-    //Tiempo ajustado a 1 segundo de simulación
-    for(int32 i=0; i<60; ++i)
+    for (int i = 0; i < 60; ++i)
     {
-        world.Step(timeStep, velocityIterations, positionIteration);
-        b2Vec2 position = body->GetPosition();
-        float angle = body->GetAngle();
-        printf("%4.2f %4.2f %4.2f \n",position.x, position.y, angle);
+        world.Step(timeStep, velocityIterations, positionIterations);
+        std::cout << body->GetPosition().x << " " << body->GetPosition().y << std::endl;
+
     }
+
+    // Remember to clean up
+    world.DestroyBody(body);
+
+    return 0;
 }
