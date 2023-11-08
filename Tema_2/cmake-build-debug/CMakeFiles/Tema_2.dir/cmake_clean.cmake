ed3740a96447d0176e8b7af395f77febf372f8d6
ProReaps <131933175+ProReaps@users.noreply.github.com>
2023-11-07 23:01:57 -0800

    Inicio del Examen parcial 2

---
+++ Tema_2/cmake-build-debug/CMakeFiles/Tema_2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tema_2.dir/main.cpp.obj"
+  "CMakeFiles/Tema_2.dir/main.cpp.obj.d"
+  "Tema_2.exe"
+  "Tema_2.exe.manifest"
+  "Tema_2.lib"
+  "Tema_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tema_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Tema_2/cmake-build-debug/CMakeFiles/Tema_2.dir/depend.make
@@ -0,0 +1,2 @@
+﻿# Empty dependencies file for Tema_2.
+# This may be replaced when dependencies are built.
